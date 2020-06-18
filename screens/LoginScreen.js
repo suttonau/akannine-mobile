d91585411a1698824ab7bed04e9e8a059e337db5
Sean Sutton <sutton_mjbc@icloud.com>
2020-06-18 13:34:50 -0400

    initial login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const LoginScreen = () => {
+  return (
+    <View style={}>
+    <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
