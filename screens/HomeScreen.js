57e8b7e3b212a7b687fa906f0bba8ef6ef0e3312
Sean Sutton <sutton_mjbc@icloud.com>
2020-06-18 13:35:56 -0400

    initial signup and home screens

---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View style={}>
+    <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
---
+++ screens/SignUpScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const SignUpScreen = () => {
+  return (
+    <View style={}>
+    <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+export default SignUpScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
