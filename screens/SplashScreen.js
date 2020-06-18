864804a98b24248a2c40e16345bd270ffee9d8a2
Sean Sutton <sutton_mjbc@icloud.com>
2020-06-18 13:36:27 -0400

    initial SplashScreen

---
+++ screens/SplashScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const SplashScreen = () => {
+  return (
+    <View style={}>
+    <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
