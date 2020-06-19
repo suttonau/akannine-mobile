c25e24b5ea5373189537d81bc27a2c2fce732d4e
Sean Sutton <sutton_mjbc@icloud.com>
2020-06-19 13:56:49 -0400

    root stack screen

--- screens/RootStackScreen.js
+++ screens/RootStackScreen.js
@@ -1,2 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+
+import SplashScreen from './SplashScreen'
+import LoginScreen from './LoginScreen'
+import SignUpScreen from './SignUpScreen'
