99bdb25716ffe19e7aefad87fcfdb61277ca73af
Sean Sutton <sutton_mjbc@icloud.com>
2020-06-23 16:09:33 -0400

    initial header/footer of splash screen

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -4,7 +4,13 @@ import { View, Text, Button, StyleSheet, Dimensions } from 'react-native'
 const SplashScreen = () => {
   return (
     <View style={styles.container}>
-    <Text>Splash Screen</Text>
+      <View style={styles.header}>
+        <Text>Splash Screen</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text>Footer</Text>
+      </View>
     </View>
   )
 }
